b4068dd0bf9b9304236471ce8a7a1aee15891245
Jasper Jiang <williammilefer@gmail.com>
2024-12-23 00:12:45 +0800

    build: 编译完成

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,10 @@ set_encodings("utf-8")
 set_warnings("all")
 set_languages("cxx17")
 
+-- 确保全局编译和链接选项添加了 -pthread
+add_cxxflags("-pthread")  -- 编译时加上 -pthread
+add_ldflags("-pthread")   -- 链接时加上 -pthread
+
 target("test")
     set_kind("static")
     add_defines(string.format("__XMAKE__=\"%s\"", os.scriptdir():gsub("\\", "/")))
